c3ebf7eba3a44b0769509c06d0cbff8f55c4fd95
gavrushuk <havryschuk@gmail.com>
2022-12-21 13:34:10 +0200

    init christmas card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import MoonImg from "./images/moon.svg";
+import Tree1Img from "./images/tree1.svg";
+import Tree2Img from "./images/tree2.svg";
+import Tree3Img from "./images/tree3.svg";
+import Paper1Img from "./images/paper1.svg";
+import Paper2Img from "./images/paper2.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="christmas-card">
+      <img className="moon-img" src={MoonImg} alt="Moon" />
+      <img className="tree-1-img" src={Tree1Img} alt="Tree 1" />
+      <img className="tree-2-img" src={Tree2Img} alt="Tree 2" />
+      <img className="tree-3-img" src={Tree3Img} alt="Tree 3" />
+      <img className="paper-1-img" src={Paper1Img} alt="Paper 1" />
+      <img className="paper-2-img" src={Paper2Img} alt="Paper 2" />
     </div>
   );
 }
